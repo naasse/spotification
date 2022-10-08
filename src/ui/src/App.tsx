dfb1121cdcd1fef4d53885da2b7008f73ac8444c
Nathan Asselstine <nate.asselstine@gmail.com>
2022-10-08 19:16:39 -0400

    feat: initial call to spotify API from React

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -1,26 +1,62 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { isNil } from "lodash";
+import SpotifyWebApi from "spotify-web-api-js";
+
 import "./App.css";
 
-function App() {
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const Main = () => {
+  const location = useLocation();
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [me, setMe] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isNil(accessToken)) {
+      if (location.hash.indexOf("#access_token") > -1) {
+        setAccessToken(location.hash.substring("#access_token=".length));
+      } else {
+        window.location.href = "/login.html";
+      }
+    }
+  }, [accessToken, location]);
+
+  useEffect(() => {
+    if (!isNil(accessToken)) {
+      console.log(accessToken);
+      const api = new SpotifyWebApi();
+      api.setAccessToken(accessToken);
+      api
+        .getMe()
+        .then((resp) => {
+          setMe(JSON.stringify(resp, undefined, 2));
+          console.log(resp);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [accessToken]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{me}</header>
     </div>
   );
-}
+};
 
 export default App;
