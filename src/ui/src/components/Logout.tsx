9998ec4f65bdc36d0542f7f62dcaaa1d954ca2a1
Nathan Asselstine <nate.asselstine@gmail.com>
2022-10-09 08:10:24 -0400

    refactor: remove old src dir

--- src/ui/src/components/Logout.tsx
+++
@@ -1,9 +0,0 @@
-export const Logout = () => {
-  localStorage.removeItem("accessToken");
-
-  return (
-    <div>
-      You have been logged out. Click <a href="/login">here</a> to log back in.
-    </div>
-  );
-};
--- src/ui/src/state/UserContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { PrivateUser } from "spotify-web-api-ts/types/types/SpotifyObjects";
-
-export type UserContextType = {
-  user?: PrivateUser;
-  token?: string | null;
-};
-
-export const UserContext = createContext<UserContextType>({});
